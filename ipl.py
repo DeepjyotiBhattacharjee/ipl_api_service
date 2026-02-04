99971403a76fcc965f81a801b6217aacc62d431f
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2026-02-04 19:19:40 +0530

    handling incorrect team

--- ipl.py
+++ ipl.py
@@ -12,15 +12,21 @@ def teams_api():
     return teams_dict
 
 def team_vs_team(team1,team2):
-  team1vteam2 = matches[((matches['Team1'] == team1) & (matches['Team2'] == team2)) | ((matches['Team1'] == team2) & (matches['Team2'] == team1))]
-  total_matches = team1vteam2.shape[0]
-  team1_wins = team1vteam2['WinningTeam'].value_counts()[team1]
-  team2_wins = team1vteam2['WinningTeam'].value_counts()[team2]
-  draws = total_matches - (team1_wins + team2_wins)
+  teams = list(set(list(matches['Team1'].unique()) + list(matches['Team2'].unique())))
+  if team1 in teams and team2 in teams:
+    team1vteam2 = matches[((matches['Team1'] == team1) & (matches['Team2'] == team2)) | ((matches['Team1'] == team2) & (matches['Team2'] == team1))]
+    total_matches = team1vteam2.shape[0]
+    team1_wins = team1vteam2['WinningTeam'].value_counts()[team1]
+    team2_wins = team1vteam2['WinningTeam'].value_counts()[team2]
+    draws = total_matches - (team1_wins + team2_wins)
 
-  return {
-      'total_matches' : total_matches,
-      f'{team1}_wins': int(team1_wins),
-      f'{team2}_wins':int(team2_wins),
-      'draws':int(draws)
-  }
+    return {
+        'total_matches' : total_matches,
+        f'{team1}_wins': int(team1_wins),
+        f'{team2}_wins':int(team2_wins),
+        'draws':int(draws)
+    }
+  else:
+     return {
+        'message':'Invalid team name'
+     }
