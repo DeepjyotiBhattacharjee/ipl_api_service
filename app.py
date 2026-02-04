8a6827e3939b115e6983aea6273b85ac9bd8d568
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2026-02-04 18:52:46 +0530

    team vs team endpoint added

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+import ipl
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+@app.route('/api/teams')
+def teams():
+    teams = ipl.teams_api()
+    return jsonify(teams)
+
+@app.route('/api/teamvteam')
+def teamvteam():
+    team1 = request.args.get('team1')
+    team2 = request.args.get('team2')
+    response = ipl.team_vs_team(team1,team2)
+    return response
+
+
+app.run(debug=True)
---
+++ ipl.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+matches = pd.read_csv("ipl-matches.csv")
+print(matches.head())
+
+def teams_api():
+    teams = list(set(list(matches['Team1'].unique()) + list(matches['Team2'].unique())))
+    teams_dict = {
+        'teams':teams
+    }
+    return teams_dict
+
+def team_vs_team(team1,team2):
+  team1vteam2 = matches[((matches['Team1'] == team1) & (matches['Team2'] == team2)) | ((matches['Team1'] == team2) & (matches['Team2'] == team1))]
+  total_matches = team1vteam2.shape[0]
+  team1_wins = team1vteam2['WinningTeam'].value_counts()[team1]
+  team2_wins = team1vteam2['WinningTeam'].value_counts()[team2]
+  draws = total_matches - (team1_wins + team2_wins)
+
+  return {
+      'total_matches' : total_matches,
+      f'{team1}_wins': int(team1_wins),
+      f'{team2}_wins':int(team2_wins),
+      'draws':int(draws)
+  }
